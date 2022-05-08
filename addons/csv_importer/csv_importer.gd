9c40ffa1ae392876be7417ba36a289577308244e
Max Leidl <38489154+mleidl@users.noreply.github.com>
2022-05-08 18:33:15 +0200

    Update csv_importer.gd

--- addons/csv_importer/csv_importer.gd
+++ addons/csv_importer/csv_importer.gd
@@ -6,14 +6,14 @@ var import_plugin
 
 
 func _enter_tree():
-    import_plugin = preload("import_plugin.gd").new()
-    add_import_plugin(import_plugin)
-
     add_custom_type(
         "CSVData", "Resource",
         preload("res://addons/csv_importer/csv_data.gd"),
         null
     )
+    
+    import_plugin = preload("import_plugin.gd").new()
+    add_import_plugin(import_plugin)
 
 
 func _exit_tree():
